58cf0d54cb93e19940a46e4b5a11ef6f0125eea4
Jyotirmoy17 <93469556+Jyotirmoy17@users.noreply.github.com>
2025-08-23 22:26:14 +0530

    Update setup.py
    Minimal

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
